585c3d4b82b11a0f319d5652e92998cb47f98832
Sawo <saworieza@gmail.com>
2018-09-16 18:10:27 +0300

    changing order in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+milestones = Milestone.create([{ phase: 'One'}, { phase: 'Two'}, { phase: 'Three'}, { phase: 'Four'}])
+stages = Stage.create([{milestone_id: 1, percentage: 30}, {milestone_id: 1, percentage: 55}, {milestone_id: 1, percentage: 15}, {milestone_id: 2, percentage: 30}, {milestone_id: 2, percentage: 27.5}, {milestone_id: 2, percentage: 27.5}, {milestone_id: 3, percentage: 90}, {milestone_id: 3, percentage: 10}, {milestone_id: 4, percentage: 100}])
+regions = Region.create([{ name: 'Nairobi'}, { name: 'Central'}, { name: 'Eastern'}, { name: 'North Eastern'}, { name: 'Coast'}, { name: 'Rift Valley'}, { name: 'Nyanza'}, { name: 'Western'}])
+towns = Town.create([{ name: "Githurai", classification: "Urban", region_id: 1}, {name: "Rongai", classification: "Urban", region_id: 6}, {name: "Kileleshwa", classification: "Urban", region_id: 1}, {name: "Kisumu", classification: "Urban", region_id: 6},  {name: "Migori", classification: "Rural", region_id: 7}])
+scopes = Scope.create([{scope_type: "Scope 1"}, {scope_type: "Scope 2"}, {scope_type: "Scope 3"}])
+statuses = Status.create([{state: 'Active'}, {state: 'Inactive'}, {state: 'Dormant'}, {state: 'Banned'}])
+
 accounts = Account.create([{name: 'Safaricom', description: "Lorem Ipsum dolor sit amet."}, {name: 'Telkom', description: "Lorem Ipsum dolor sit amet."}, {name: 'Airtel', description: "Lorem Ipsum dolor sit amet."}])
 costcenters = Costcenter.create([{number: 123456789, details: "Lorem Ipsum dolor sit amet.", account_id: 1 }, {number: 1234556487, details: "Lorem Ipsum dolor sit amet.", account_id: 1 }, {number: 1298566789, details: "Lorem Ipsum dolor sit amet.", account_id: 2 }, {number: 9965456789, details: "Lorem Ipsum dolor sit amet.", account_id: 3 }
-statuses = Status.create([{state: 'Active'}, {state: 'Inactive'}, {state: 'Dormant'}, {state: 'Banned'}])
-scopes = Scope.create([{scope_type: "Scope 1"}, {scope_type: "Scope 2"}, {scope_type: "Scope 3"}])
 sites = Site.create([{ name: "Githurai", number: 12, tower_height: 12, status_id: 1, scope_id: 1, town_id: 1, region_id: 2}, { name: "Rongai", number: 12, tower_height: 12, status_id: 1, scope_id: 1, town_id: 2, region_id: 1}])
 customerquotes = Customerquote.create([{name: "Quote for Githurai", date: "2018-09-16", amount: 10202, costcenter_id: 1, scope_id: 1, site_id: 1}, {name: "Quote for Rongai", date: "2018-09-16", amount: 103502, costcenter_id: 1, scope_id: 2, site_id: 2}])	
 customerpos = Customerpo.create([{number: 24556521, date: "2018-09-16", amount: 10000, customerquote_id: 1, scope_id: 1, site_id: 1}, {number: 245545621, date: "2018-09-16", amount: 10100, customerquote_id: 2, scope_id: 2, site_id: 2}])
@@ -19,7 +25,3 @@ contractorpos = Contractorpo.create([{number: 12345678, date: "2018-09-16", amou
 contractorinvoices = Contractorinvoice.create([{number: 1234564, date: "2018-09-16", amount: 5687985, milestone_id: 1, site_id: 1, stage_id: 1, contractorpo_id: 1}, {number: 55524564, date: "2018-11-16", amount: 5687985, milestone_id: 2, site_id: 2, stage_id: 2, contractorpo_id: 2}])
 paymentvouchers = paymentvoucher.create([{number: 564879, date: "2018-10-22", amount: 5648955, contractor_id: 1, milestone_id: 1, stage_id: 1, site_id: 1, contractorinvoice_id: 1}, {number: 564879, date: "2018-10-22" , amount: 5648955, contractor_id: 3, milestone_id: 1, stage_id: 3, site_id: 2, contractorinvoice_id: 2}])
 
-milestones = Milestone.create([{ phase: 'One'}, { phase: 'Two'}, { phase: 'Three'}, { phase: 'Four'}])
-stages = Stage.create([{milestone_id: 1, percentage: 30}, {milestone_id: 1, percentage: 55}, {milestone_id: 1, percentage: 15}, {milestone_id: 2, percentage: 30}, {milestone_id: 2, percentage: 27.5}, {milestone_id: 2, percentage: 27.5}, {milestone_id: 3, percentage: 90}, {milestone_id: 3, percentage: 10}, {milestone_id: 4, percentage: 100}])
-regions = Region.create([{ name: 'Nairobi'}, { name: 'Central'}, { name: 'Eastern'}, { name: 'North Eastern'}, { name: 'Coast'}, { name: 'Rift Valley'}, { name: 'Nyanza'}, { name: 'Western'}])
-towns = Town.create([{ name: "Githurai", classification: "Urban", region_id: 1}, {name: "Rongai", classification: "Urban", region_id: 6}, {name: "Kileleshwa", classification: "Urban", region_id: 1}, {name: "Kisumu", classification: "Urban", region_id: 6},  {name: "Migori", classification: "Rural", region_id: 7}])
