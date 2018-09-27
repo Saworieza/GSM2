9f511c34b03b8e23a8e1edef6ada88e1e45506eb
Sawo <saworieza@gmail.com>
2018-09-27 12:38:09 +0300

    fixed some ommisions in invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,10 +1,10 @@
 class Invoice < ApplicationRecord
 	# is_impressionable
-  belongs_to :status
-  belongs_to :scope
+  # belongs_to :status
+  # belongs_to :scope
   belongs_to :customerpo
   belongs_to :customerquote
-  belongs_to :site
+  # belongs_to :site
   belongs_to :milestone
   belongs_to :stage
 
