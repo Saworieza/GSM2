a68135d460f38df3db820b52bc21161b1e0b8398
Sawo <saworieza@gmail.com>
2018-12-03 10:27:56 +0300

    changes to payment voucher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,12 +63,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
+    # disabled temporarily to work localy
+    # address: “smtp.gmail.com”,
     port: 587,
-    domain: ENV[“GMAIL_DOMAIN”],
-    authentication: “plain”,
-    enable_starttls_auto: true,
-    user_name: ENV[“GMAIL_USERNAME”],
-    password: ENV[“GMAIL_PASSWORD”]
+    # domain: ENV[“GMAIL_DOMAIN”],
+    # authentication: “plain”,
+    # enable_starttls_auto: true,
+    # user_name: ENV[“GMAIL_USERNAME”],
+    # password: ENV[“GMAIL_PASSWORD”]
   }
 end
